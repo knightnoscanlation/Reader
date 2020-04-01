76ee10a64469f3e2d48d7f3f6f235b0b9e34e0e0
Knight No Scanlation <60488087+knightnoscanlation@users.noreply.github.com>
2020-04-01 15:14:16 -0500

    Creacion de info.php
    Archivo para guardado de información por ahora.
    Después se sacara desde la misma DB

---
+++ info.php
@@ -0,0 +1,9 @@
+<?
+$kyoskomsg = "Kyosko el Vejestorio";
+
+$txts = array(
+    "InfoKnight" => "Scanlation desde el 2015 traduciendo y editando mangas/manhwas/manhuas para ustedes",
+    "Publicaciones" => "No hay fecha de publicacion para cada serie excepto algunas como Solo Leveling"
+)
+
+?>
